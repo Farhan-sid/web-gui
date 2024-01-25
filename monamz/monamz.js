a64ef325697ce6afa670a999af99003fa053aea1
Farhan-sid <fs440920@gmail.com>
2024-01-25 17:40:23 +0500

    monamz

---
+++ monamz/monamz.js
@@ -0,0 +1,6 @@
+var sea =document.querySelector(".search")
+var dn = document.querySelector(".dn");
+sea.addEventListener("click",function(){
+    dn.style.opacity = 1
+    dn.style.visibility = visible
+})
