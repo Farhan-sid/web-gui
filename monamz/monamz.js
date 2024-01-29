7b8563ab718dd47d7e6c83a4b03243814d7842f2
Farhan-sid <fs440920@gmail.com>
2024-01-29 17:04:34 +0500

    monamz

--- monamz/monamz.js
+++ monamz/monamz.js
@@ -1,6 +1,17 @@
 var sea =document.querySelector(".search")
 var dn = document.querySelector(".dn");
+var cd = document.querySelector(".cd")
+var mo = document.querySelector(".mask-overlay")
 sea.addEventListener("click",function(){
     dn.style.opacity = 1
-    dn.style.visibility = visible
+    dn.style.visibility = 'visible'
+    mo.style.opacity =1
+    mo.style.visibility = 'visible'
+})
+
+cd.addEventListener("click",function(){
+    dn.style.opacity = 0
+    dn.style.visibility = 'hidden'
+    mo.style.opacity = 0
+    mo.style.visibility = 'hidden'
 })
