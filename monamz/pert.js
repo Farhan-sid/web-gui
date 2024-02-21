67ab020fda4c4be322e89551b7c4e7eb3a594607
Farhan-sid <fs440920@gmail.com>
2024-02-21 17:09:42 +0500

    monamz

--- monamz/pert.js
+++ monamz/pert.js
@@ -2,6 +2,14 @@ var s_p = document.querySelector(".shortby_pick")
 var w_s = document.querySelector(".wrap_sortby")
 var svg = document.querySelector(".ic_triangle_svg")
 var re = document.querySelector(".remove")
+var fil = document.querySelector(".cat_filter")
+var clo = document.querySelector(".close_pp")
+var mo = document.querySelector(".mask-overlay")
+var pf = document.querySelector(".popup_filter")
+var body = document.querySelector("body")
+var sea =document.querySelector(".search")
+var dn = document.querySelector(".dn");
+var cd = document.querySelector(".cd")
 
 var isHidden = false;
 
@@ -28,5 +36,36 @@ s_p.addEventListener("click", function() {
     isHidden = !isHidden;
 });
 
+fil.addEventListener("click",function(){
+    pf.style.opacity = 1
+    pf.style.visibility = 'visible'
+    mo.style.opacity = 1
+    mo.style.visibility = 'visible'
+    body.style.overflowY = "hidden"
+})
+
+clo.addEventListener("click",function(){
+    pf.style.opacity = 0
+    pf.style.visibility = 'hidden'
+    mo.style.opacity = 0
+    mo.style.visibility = 'hidden'
+    body.style.overflowY = "visible"
+})
+
+sea.addEventListener("click",function(){
+    dn.style.opacity = 1
+    dn.style.visibility = 'visible'
+    mo.style.opacity = 1
+    mo.style.visibility = 'visible'
+    body.style.overflowY = "hidden"
+})
+
+cd.addEventListener("click",function(){
+    dn.style.opacity = 0
+    dn.style.visibility = 'hidden'
+    mo.style.opacity = 0
+    mo.style.visibility = 'hidden'
+    body.style.overflowY = "visible"
+})
 
 
